0bc16e5d17221c01e2a260977733d3f6be5811f3
idcetim <javirofe96@gmail.com>
2023-06-24 10:51:29 +0200

    Add 03-PasswordGenerator challenge [WIP]
    It is almost finished. It generates the password. However, if all the conditions are true, it is possible that some of the conditions will not included in the ending password because all the elements of that conditions could be replaced

--- challenges/03-PasswordGenerator.js
+++ challenges/03-PasswordGenerator.js
@@ -7,31 +7,60 @@
  * - Con o sin símbolos.
  * (Pudiendo combinar todos estos parámetros entre ellos)
  */
+const lowerCasses = "abcdefghijklmnopqrstuvwxyz"
+const lowerUpperCases = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const symbols = "*+#!¿=&%¡?"
 
 function generateRandomString(length, combineUpperLowerCase) {
-  const lowerCasses = "abcdefghijklmnopqrstuvwxyz"
-  const lowerUpperCases = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  const randomString = ""
+  let randomString = ""
   const charactersToUse = combineUpperLowerCase ? lowerUpperCases : lowerCasses
-  for (let i = 0; i > length; i++) {
+  for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * charactersToUse.length)
     randomString += charactersToUse[randomIndex]
   }
   return randomString
 }
 
-function addRandonNumber(passwordBase) {
+function addRandonNumbers(passwordBase) {
   let passWordWithNumber = [...passwordBase].join("")
   const numbersToInclude = Math.floor(Math.random() * passWordWithNumber.length)
-  const numbersToAdd = []
-  const indexToModify = []
   for (let i = 0; i < numbersToInclude; i++) {
-    numbersToAdd.push(Math.floor(Math.random() * 10))
+    const numberToAdd = Math.floor(Math.random() * 10)
+    const indexToModify = Math.floor(Math.random() * passwordBase.length)
+    passWordWithNumber = passWordWithNumber.substring(0, indexToModify) + numberToAdd + passWordWithNumber.substring(indexToModify + 1)
   }
+  return passWordWithNumber
 }
 
-function generatePassword(length, combineUpperLowerCase, numberIsIncluded, simbolIsIncluded) {
+function addSymbol(passwordBase) {
+
+  let passwrodWithSymbol = [...passwordBase].join("")
+  const numberOfSymbolsToInclude = Math.floor(Math.random() * passwrodWithSymbol.length)
+  for (let i = 0; i < numberOfSymbolsToInclude; i++) {
+    const randomSymbol = symbols[Math.floor(Math.random() * symbols.length)]
+    const indexToModify = Math.floor(Math.random() * passwordBase.length)
+    passwrodWithSymbol = passwrodWithSymbol.substring(0, indexToModify) + randomSymbol + passwrodWithSymbol.substring(indexToModify + 1)
+  }
+  return passwrodWithSymbol
+}
+
+function generatePassword(length, combineUpperLowerCase = false, numberIsIncluded = false, symbolIscluded = false) {
   if (length < 8 || length > 16) throw new Error("Imposible to create a password with length lower than 8 or higher than 16")
   let passwordBase = generateRandomString(length, combineUpperLowerCase)
-  if (numberIsIncluded) {}
+  const passwordAfterNumber = !numberIsIncluded ? [...passwordBase].join('') : addRandonNumbers(passwordBase)
+  const password = !symbolIscluded ? [...passwordAfterNumber].join('') : addSymbol(passwordAfterNumber)
+  return password
+
+}
+
+try {
+  const NUMBER_PASSWORDS = 5
+  for (let i = 0; i < NUMBER_PASSWORDS; i++) {
+    console.log(`Password ${i}:`)
+    const password = generatePassword(12, true, true, true)
+    console.log(password)
+  }
+
+} catch (error) {
+  console.error(error.message)
 }
